e9652e08a2eee404b9b23660040164e1da85d064
Jonas Tänzer <taenzer.jonas@gmx.de>
2026-03-27 13:28:28 +0100

    Beziehungen zwischen Modellen definiert

--- job-portal/app/Models/Job.php
+++ job-portal/app/Models/Job.php
@@ -7,4 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Job extends Model
 {
     protected $table = 'job_posts';
+    public function company()
+{
+    return $this->belongsTo(Company::class);
+}
+
+public function category()
+{
+    return $this->belongsTo(Category::class);
+}
+
+public function user()
+{
+    return $this->belongsTo(User::class);
+}
 }
